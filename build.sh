b9d808d970483d78a12267c326cf00aed34ed126
Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>
2017-04-18 16:15:55 +0800

    try

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ emerge $opts --sync
 emerge $opts --unmerge sys-fs/eudev
 echo ">=sys-apps/dbus-1.10.12 systemd"  > /etc/portage/package.use/iputils
 emerge $opts --pretend --tree --verbose sys-apps/systemd
-emerge $opts --noreplace sys-apps/systemd
+emerge $opts --noreplace --keep-going sys-apps/systemd
 
 end=$(TZ="Asia/Taipei" date --iso-8601=second)
 
