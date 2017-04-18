b286bce8126a5743f09ba99a1c7e03bffa3e1867
Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>
2017-04-18 14:19:42 +0800

    less verbose

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
-emerge --sync
-emerge --unmerge sys-fs/eudev
+beg=$(TZ="Asia/Taipei" date --iso-8601=second)
+
+opts=""
+opts="$opts --quiet"
+emerge $opts --sync
+emerge $opts --unmerge sys-fs/eudev
 echo ">=sys-apps/dbus-1.10.12 systemd"  > /etc/portage/package.use/iputils
-emerge --quiet sys-apps/systemd
+emerge $opts --pretend --tree sys-apps/systemd
+emerge $opts sys-apps/systemd
+
+end=$(TZ="Asia/Taipei" date --iso-8601=second)
+
+echo "beg: $beg"
+echo "end: $end"
