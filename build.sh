6e3c2c988f8bbec6edca02969d1cb6f89e8f8003
Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>
2017-04-19 19:50:15 +0800

    make it better

--- build.sh
+++ build.sh
@@ -9,12 +9,14 @@ emerge $opts --unmerge sys-fs/eudev
 echo ">=sys-apps/dbus-1.10.12 systemd"  > /etc/portage/package.use/iputils
 echo "sys-libs/pam" > /etc/portage/package.mask/pam
 emerge $opts --pretend --tree --verbose sys-apps/systemd
-
 emerge $opts sys-apps/systemd
-for (( x=1; x<=2; x++ ))
-do
-    emerge $opts --keep-going --skip-first sys-apps/systemd
-done
+
+qlist -Iv sys-apps/systemd || {
+    for (( x=1; x<=2; x++ ))
+    do
+        emerge $opts --keep-going --skip-first sys-apps/systemd
+    done
+}
 
 end=$(TZ="Asia/Taipei" date --iso-8601=second)
 
