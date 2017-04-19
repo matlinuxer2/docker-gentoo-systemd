550e987a34cf85bcc02d92b7b0196da0f02a410a
Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>
2017-04-19 18:06:45 +0800

    try

--- build.sh
+++ build.sh
@@ -7,11 +7,11 @@ opts="$opts --quiet"
 emerge $opts --sync
 emerge $opts --unmerge sys-fs/eudev
 echo ">=sys-apps/dbus-1.10.12 systemd"  > /etc/portage/package.use/iputils
-echo "sys-libs/pam" > /etc/portage/package.mask
+echo "sys-libs/pam" > /etc/portage/package.mask/pam
 emerge $opts --pretend --tree --verbose sys-apps/systemd
 
 emerge $opts sys-apps/systemd
-for (( x=1; x<=3; x++ ))
+for (( x=1; x<=2; x++ ))
 do
     emerge $opts --keep-going --skip-first sys-apps/systemd
 done
